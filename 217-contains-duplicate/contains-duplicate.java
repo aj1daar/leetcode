774db8f04cfc1a8ebe1c12497c96a3fe0a360cba
Atai Nogoibaev <atainogoibaev@gmail.com>
2026-07-09 13:10:43 +0200

    fix: remove 217-contains-duplicate problem files and README

--- 217-contains-duplicate/contains-duplicate.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> mySet = new HashSet<Integer>();
-        for (int i: nums) mySet.add(i);
-        if (mySet.size() >= nums.length) return false;
-        else return true;
-
-    }
-}
